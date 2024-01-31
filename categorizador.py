3fb55edd774d1e4c557db7c4025bf52d45101646
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2024-01-31 13:11:21 -0400

    Criando função para encpisular o código

--- categorizador.py
+++ categorizador.py
@@ -7,50 +7,50 @@ load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = 'gpt-4'
 
-prompt_sistema = f"""
-        Você é um categorizador de produtos.
-        Você deve assumir as categorias presentes na lista abaixo.
 
-        # Lista de Categorias Válidas
-            - Moda Sustentável
-            - Produtos para o Lar
-            - Beleza Natural
-            - Eletrônicos Verdes
-            - Higiene Pessoal
+def categorizar_produto(nome_produto, lista_categorias_validas):
+        prompt_sistema = f"""
+                Você é um categorizador de produtos.
+                Você deve assumir as categorias presentes na lista abaixo.
 
-        # Formato da Saída
-        Produto: Nome do Produto
-        Categoria: apresente a categoria do produto
+                # Lista de Categorias Válidas
+                {lista_categorias_validas.split(",")}
 
-        # Exemplo de Saída
-        Produto: Escova elétrica com recarga solar
-        Categoria: Eletrônicos Verdes
-    """
+                # Formato da Saída
+                Produto: Nome do Produto
+                Categoria: apresente a categoria do produto
 
-prompt_usuario = input("Apresente o nome de um produto: ")
+                # Exemplo de Saída
+                Produto: Escova elétrica com recarga solar
+                Categoria: Eletrônicos Verdes
+        """
 
+        """
+        O que esse categorizador vai fazer? Assumiremos que temos o seguinte problema: 
+        receberemos um produto e precisaremos informar a qual categoria de produtos ele pertence.
+        Para isso, usaremos IA generativa para analisar o contexto e definir qual categoria ele pode ser melhor associado.
+        """
+        resposta = cliente.chat.completions.create(
+                messages=[
+                        {
+                                "role":"system",
+                                "content": prompt_sistema
+                        },
+                        {
+                                "role" : "user",
+                                "content": nome_produto
+                        }
+                ],
+                model=modelo,
+                temperature=0,
+                max_tokens=200,
+        )
 
+        return resposta.choices[0].message.content
 
-"""
-O que esse categorizador vai fazer? Assumiremos que temos o seguinte problema: 
-receberemos um produto e precisaremos informar a qual categoria de produtos ele pertence.
-Para isso, usaremos IA generativa para analisar o contexto e definir qual categoria ele pode ser melhor associado.
-"""
+categorias_validas = input("Digite as categorias válidas: ")
 
-resposta = cliente.chat.completions.create(
-        messages=[
-                {
-                        "role":"system",
-                        "content": prompt_sistema
-                },
-                {
-                        "role" : "user",
-                        "content": prompt_usuario
-                }
-        ],
-        model=modelo,
-        temperature=0,
-        max_tokens=200,
-)
-
-print(resposta.choices[0].message.content)
+while(True):
+    nome_produto = input("Digite o nome de um produto: ")
+    resposta = categorizar_produto(nome_produto, categorias_validas)
+    print (resposta)
